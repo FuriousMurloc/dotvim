18b81d49103ffae9d928e1891f0c5d1f60b2979e
aleix <aleix.miret@gmail.com>
2025-12-24 01:04:37 +0100

    desfuncionalitzar la config

--- init.lua
+++ init.lua
@@ -1,95 +1,141 @@
-function Editor_config()
-    vim.g.mapleader = ' '
+vim.g.mapleader = ' '
 
-    -- Tabs stuff
-    vim.opt.tabstop = 4
-    vim.opt.softtabstop = 4
-    vim.opt.shiftwidth = 4
-    vim.opt.expandtab = true
+-- Tabs stuff
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
-    -- numberline stuff
-    vim.opt.number = true
-    vim.opt.relativenumber = true
-    vim.opt.signcolumn = 'yes'
+-- numberline stuff
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
 
-    -- more general stuff
-    vim.opt.termguicolors = true
-    vim.opt.wrap = true
-    vim.opt.swapfile = false
-    vim.opt.winborder = 'rounded'
-    vim.opt.clipboard = 'unnamedplus'
+-- more general stuff
+vim.opt.termguicolors = true
+vim.opt.wrap = true
+vim.opt.swapfile = false
+vim.opt.winborder = 'rounded'
+vim.opt.clipboard = 'unnamedplus'
 
-    vim.g.vim_ui_open_cmd = 'gio open'
+vim.opt.mouse = ""
+vim.g.vim_ui_open_cmd = 'gio open'
 
-    vim.cmd('set completeopt+=noselect,menuone,popup')
+vim.cmd('set completeopt+=noselect,menuone,popup')
 
-    -- reload this file
-    vim.keymap.set('n', '<leader>+', ':update<CR> :source<CR>')
-    -- exit terminal with esc
-    vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>')
-end
+-- reload this file
+vim.keymap.set('n', '<leader>+', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, { desc = 'vim.lsp.buf.definition' })
+vim.keymap.set('n', '<leader>fo', vim.lsp.buf.format, { desc = 'vim.lsp.buf.format' })
+-- exit terminal with esc
+vim.keymap.set('t', '<esc>', '<c-\\><c-n>', { desc = 'esc in terinal mode.' })
 
-function Package_management()
-    vim.pack.add({
-        { src = 'https://github.com/catppuccin/nvim.git' },
-        { src = 'https://github.com/neovim/nvim-lspconfig.git' },
-        { src = 'https://github.com/echasnovski/mini.pick' },
-        { src = 'https://github.com/echasnovski/mini.files' },
-        { src = 'https://github.com/zk-org/zk-nvim' },
-        { src = '/home/aleix/Documents/floaterminal' },
-    })
-    Mini_pick = require('mini.pick')
-    Mini_files = require('mini.files')
-    Lsp_config = require('lspconfig')
-    Floaterminal = require('floaterminal')
-    Catppuccin = require("catppuccin")
-end
+vim.pack.add({
+    { src = 'https://github.com/catppuccin/nvim.git' },
+    { src = 'https://github.com/neovim/nvim-lspconfig.git' },
+    { src = 'https://github.com/echasnovski/mini.pick' },
+    { src = 'https://github.com/echasnovski/mini.files' },
+    { src = 'https://github.com/zk-org/zk-nvim' },
+    { src = '/home/aleix/Documents/floaterminal' },
+})
+Mini_pick = require('mini.pick')
+Mini_files = require('mini.files')
+Lsp_config = require('lspconfig')
+Floaterminal = require('floaterminal')
+Catppuccin = require("catppuccin")
+Zk = require("zk")
+Zk.setup({
+    picker = "minipick",
+    lsp = {
+        -- `config` is passed to `vim.lsp.start_client(config)`
+        config = {
+            cmd = { "zk", "lsp" },
+            name = "zk",
+            -- on_attach = ...
+            -- etc, see `:h vim.lsp.start_client()`
+        },
+        -- automatically attach buffers in a zk notebook that match the given filetypes
+        auto_attach = {
+            enabled = true,
+            filetypes = { "markdown" },
+        },
+    },
+})
 
-function Configure_lsp()
-    local on_attach = function(client, bufnr)
-        vim.lsp.completion.enable(true, client.id, bufnr, {
-            autotrigger = true,
-            convert = function(item)
-                return { abbr = item.label:gsub('%b()', '') }
-            end,
-        })
-        vim.keymap.set('i', '<C-space>', vim.lsp.completion.get, { desc = 'trigger autocompletion' })
-    end
-    Lsp_config['lua_ls'].setup({ on_attach = on_attach })
-    Lsp_config['clangd'].setup({
-        cmd = { 'clangd', '--background-index', '--clang-tidy' },
-        on_attach = on_attach
-    })
-    vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { desc = 'vim.lsp.buf.format()' })
 
-    vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*",
-        callback = function(args)
-            vim.lsp.buf.format({ bufnr = args.buf })
+local on_attach = function(client, bufnr)
+    vim.lsp.completion.enable(true, client.id, bufnr, {
+        autotrigger = true,
+        convert = function(item)
+            return { abbr = item.label:gsub('%b()', '') }
         end,
     })
-    function Show_outline()
-        local params = { textDocument = { uri = vim.uri_from_bufnr(0) } }
-        vim.lsp.buf_request(0, 'textDocument/documentSymbol', params, function(_, _, result, _)
-            if result and #result > 0 then
-                local outline = {}
-                for _, symbol in ipairs(result) do
-                    table.insert(outline, symbol.name)
-                end
-                print(table.concat(outline, "\n"))
-            else
-                print("No symbols found.")
-            end
-        end)
-    end
-
-    vim.lsp.buf.document_symbol()
-    vim.lsp.enable({ "lua_ls", "clangd" })
+    vim.keymap.set('i', '<C-space>', vim.lsp.completion.get, { desc = 'trigger autocompletion' })
 end
 
-Package_management()
-Editor_config()
-Configure_lsp()
+Lsp_config['lua_ls'].setup({ on_attach = on_attach })
+
+Lsp_config['clangd'].setup({
+    cmd = { 'clangd', '--background-index', '--clang-tidy' },
+    on_attach = on_attach
+})
+
+Lsp_config['zls'].setup
+{
+    -- Server-specific settings. See `:help lspconfig-setup`
+
+    -- There are two ways to set config options:
+    --   - edit your `zls.json` that applies to any editor that uses ZLS
+    --   - set in-editor config options with the `settings` field below.
+    --
+    -- Further information on how to configure ZLS:
+    -- https://zigtools.org/zls/configure/
+    settings = {
+        zls = {
+            -- Whether to enable build-on-save diagnostics
+            --
+            -- Further information about build-on save:
+            -- https://zigtools.org/zls/guides/build-on-save/
+            -- enable_build_on_save = true,
+
+            -- Neovim already provides basic syntax highlighting
+            semantic_tokens = "partial",
+            zig_lib_path = "/home/aleix/srcs/zig/lib/"
+
+        }
+    }
+}
+
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { desc = 'vim.lsp.buf.format()' })
+
+-- MAN stuff
+vim.g.man_default_sects = '2,3'
+vim.keymap.set('n', 'mm', function()
+    local cursor = vim.fn.expand('<cword>')
+    vim.api.nvim_command(':tab Man ' .. cursor)
+end, {desc='open man on cursor'})
+
+vim.keymap.set({'n','i'}, '<C-h>', ':tabprevious<CR>')
+vim.keymap.set({'n','i'}, '<C-l>', ':tabnext<CR>')
+
+function Show_outline()
+    local params = { textDocument = { uri = vim.uri_from_bufnr(0) } }
+    vim.lsp.buf_request(0, 'textDocument/documentSymbol', params, function(_, _, result, _)
+        if result and #result > 0 then
+            local outline = {}
+            for _, symbol in ipairs(result) do
+                table.insert(outline, symbol.name)
+            end
+            print(table.concat(outline, "\n"))
+        else
+            print("No symbols found.")
+        end
+    end)
+end
+
+vim.lsp.buf.document_symbol()
+vim.lsp.enable({ "lua_ls", "clangd" })
+
 
 Mini_files.setup({ mappings = { close = '<ESC>' } })
 
@@ -106,10 +152,13 @@ vim.keymap.set('n', '<leader>fg', Mini_pick.builtin.grep_live, { desc = 'mini_pi
 vim.keymap.set('n', '<leader>fb', Mini_pick.builtin.buffers, { desc = 'mini_pick.builtin.buffers()' })
 vim.keymap.set('n', '<leader>t', Mini_files.open, { desc = 'mini_files.open()' })
 vim.keymap.set('n', '<leader>o', Floaterminal.toggle_terminal, { desc = 'floaterminal.toggle_terminal' })
+vim.keymap.set('n', '<leader>zz', Zk.edit, { desc = 'picker zk' })
 
 vim.keymap.set('n', '<leader>h', function()
     Mini_pick.builtin.help({ default_split = 'vertical' }, {})
+    vim.api.nvim_win_set_width(0,83);
 end, { desc = 'mini_pick.builtin.help()' })
+
 
 Floaterminal.setup()
 Catppuccin.setup()
